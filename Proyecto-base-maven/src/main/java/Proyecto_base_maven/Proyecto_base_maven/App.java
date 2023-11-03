57e1370a8070be67258fd2128e153f5c656da3e6
Manel Castellví Cerezuela <manelcastellvicerezuela@gmail.com>
2023-11-03 15:12:20 +0100

    Proyecto base creado maven

---
+++ Proyecto-base-maven/src/main/java/Proyecto_base_maven/Proyecto_base_maven/App.java
@@ -0,0 +1,13 @@
+package Proyecto_base_maven.Proyecto_base_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
